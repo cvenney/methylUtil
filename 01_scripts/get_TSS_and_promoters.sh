edc1c879ee7fca43832a0761f6f04b8f70ef6b51
kylewellband <kyle.wellband@gmail.com>
2019-11-11 12:21:30 -0500

    Initial commit

---
+++ 01_scripts/get_TSS_and_promoters.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# get_TSS_and_promoters.sh
+
+GFF="02_reference/genes.gff.gz"
+GENOME="02_reference/genome.fasta"
+
+if [[ $GFF == "" ]]
+then
+	echo "You forgot the input file!"
+	exit
+fi
+
+if [[ "${GFF##*.}" == "gz" ]]
+then
+	gunzip -c $GFF | awk '($1 !~ /#.*/ && $3 == "gene") {OFS="\t"} {if ($7 == "+") {print $1, $4 - 1, $4, $9, ".", $7} else if($7 == "-") {print $1, $5, $5 + 1, $9, ".", $7}}' > 05_bed_files_for_analysis/tss.bed
+else
+	cat $GFF | awk '($1 !~ /#.*/ && $3 == "gene") {OFS="\t"} {if ($7 == "+") {print $1, $4 - 1, $4, $9, ".", $7} else if($7 == "-") {print $1, $5, $5 + 1, $9, ".", $7}}' > 05_bed_files_for_analysis/tss.bed
+fi
+
+bedtools slop -i 05_bed_files_for_analysis/tss.bed -g $GENOME -l 1000 -r 200 -s > 05_bed_files_for_analysis/promotors.bed
+
