8fe4289c8606e5e6db23b1902434b0b1455a0d15
kylewellband <kyle.wellband@gmail.com>
2019-11-22 19:52:36 -0500

    Fix remove CT script

--- 01_scripts/remove_CT_SNPs_from_bedGraphs.sh
+++ 01_scripts/remove_CT_SNPs_from_bedGraphs.sh
@@ -7,8 +7,14 @@ OUTPUT="04_filtered_bedGraphs"
 NCPUS=4
 
 # Negative intersect with bedtools
-ls "$INPUT"/*.bedGraph.gz | perl -pe 's/\.bedGraph\.gz//g' |
-parallel --bar -j $NCPUS gunzip -c {}.bedGraph.gz | 
-bedtools intersect -a stdin -b 02_reference/CT_AG_snps.bed -wa -v | 
-gzip > "$OUTPUT"/{}.bedGraph.gz
+for file in $(ls "$INPUT"/*.bedGraph.gz | perl -pe 's/\.bedGraph\.gz//g')
+do
+    name=$(basename $file)
+    
+    echo "Filtering sample: $file"    
+    
+    gunzip -c "$INPUT"/"$name".bedGraph.gz | 
+    bedtools intersect -a stdin -b 02_reference/CT_AG_snps.bed -wa -v | 
+    gzip > "$OUTPUT"/"$name".bedGraph.gz
 
+done
