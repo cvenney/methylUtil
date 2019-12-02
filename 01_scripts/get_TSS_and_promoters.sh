250da0fc0d6841784d8ddcb1fcbc4efffe5d0ceb
kylewellband <kyle.wellband@gmail.com>
2019-12-02 16:18:34 -0500

    Fix naming of genes in promotors / tss script

--- 01_scripts/get_TSS_and_promoters.sh
+++ 01_scripts/get_TSS_and_promoters.sh
@@ -14,10 +14,28 @@ cut -f1,2 02_reference/genome.fasta.fai > 02_reference/genome.genome
 
 if [[ "${GFF##*.}" == "gz" ]]
 then
-	gunzip -c $GFF | awk 'BEGIN{OFS="\t"}  ($1 !~ /"#.*"/ && $3 == "gene") {if ($7 == "+") {print $1, $4 - 1, $4, $9, ".", $7} else if($7 == "-") {print $1, $5, $5 + 1, $9, ".", $7}}' > 05_bed_files_for_analysis/tss.bed
+	gunzip -c $GFF | 
+	awk -v OFS="\t" '($1 !~ /"#.*"/ && $3 == "gene") {
+	    if ($7 == "+") {
+	        print $1, $4 - 1, $4, $9, ".", $7
+	    } else if($7 == "-") {
+	        print $1, $5, $5 + 1, $9, ".", $7}}' | 
+	perl -pe 's/ID=//' | 
+	perl -pe 's/;Dbxref=GeneID//' | 
+	perl -pe 's/;.*\b//' > 05_bed_files/tss.bed
 else
-	cat $GFF | awk 'BEGIN{OFS="\t"}  ($1 !~ /"#.*"/ && $3 == "gene") {if ($7 == "+") {print $1, $4 - 1, $4, $9, ".", $7} else if($7 == "-") {print $1, $5, $5 + 1, $9, ".", $7}}' > 05_bed_files_for_analysis/tss.bed
+	cat $GFF | 
+	awk -v OFS="\t" '($1 !~ /"#.*"/ && $3 == "gene") {
+	    if ($7 == "+") {
+	        print $1, $4 - 1, $4, $9, ".", $7
+	    } else if($7 == "-") {
+	        print $1, $5, $5 + 1, $9, ".", $7}}' | 
+	perl -pe 's/ID=//' | 
+	perl -pe 's/;Dbxref=GeneID//' | 
+	perl -pe 's/;.*\b//' > 05_bed_files/tss.bed
 fi
+
+
 
 bedtools slop -i 05_bed_files/tss.bed -g $GENOME -l 1000 -r 200 -s > 05_bed_files/promotors.bed
 
