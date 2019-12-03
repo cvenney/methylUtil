c2fd9193884adf05498e2f32e4f31d523996dfbc
kylewellband <kyle.wellband@gmail.com>
2019-12-03 15:11:51 -0500

    Add summary script

---
+++ 01_scripts/util/dml_in_dmr_summary.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# dml_in_dmr_summary.sh
+
+if [ $# -ne 2 ]
+then
+    echo "Usage: $0 <DSS_ouput_basename> <DSS_ouput_settings>"
+    echo "e.g. tab complete using *dml* or *dmr* file, replace 'dml' or 'dmr' with a space..."
+    exit
+fi
+
+file=$1
+settings=$2
+
+dml=${gunzip -c "${file}dml${settings}" | awk 'END{print NR -1}')
+
+gunzip -c "${file}dmr${settings}" | 
+awk -v dml=$dml '(NR != 1) {
+    sum += $5
+} END {
+    print NR - 1 " DMRs contain " sum " (" (sum/dml*100) "%) DMLs"
+}'
