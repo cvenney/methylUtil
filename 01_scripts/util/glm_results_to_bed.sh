fe043e8f535e37d0ed133c8c83433c37e140fc21
kylewellband <kyle.wellband@gmail.com>
2019-12-03 10:16:55 -0500

    Add accessory script to create bed fie from DSS results

---
+++ 01_scripts/util/glm_results_to_bed.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# wald_results_to_bed.sh
+
+if [ $# -ne 1 ]
+then
+    echo "Usage: $0 <wald_test_DMR_file>"
+    exit
+fi
+
+outdir="05_bed_files"
+
+file=$1
+name=$(basename $(echo $file | perl -pe 's/(\.txt)(\.gz)//g'))
+
+if [ ${file%*.} == "gz" ]
+then
+    gunzip -c $file | 
+    awk -v OFS="\t" '(NR != 1){
+        print $1, $2, $3, "nCG=" $5 ";score=" $6, $6, "."
+    }' > ${outdir}/${name}.bed
+else
+    cat $file | 
+    awk -v OFS="\t" '(NR != 1){
+        print $1, $2, $3, "nCG=" $5 ";score=" $6, $6, "."
+    }' > ${outdir}/${name}.bed
+fi
+
