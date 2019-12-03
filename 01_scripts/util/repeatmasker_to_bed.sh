72c74f1e44208952752a89303787a6306db41cf1
kylewellband <kyle.wellband@gmail.com>
2019-12-03 13:17:37 -0500

    Add script to process NCBI repeatmasker files

---
+++ 01_scripts/util/repeatmasker_to_bed.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# repeatmasker_to_bed.sh
+
+if [ $# -ne 1 ]
+then
+    echo "Usage: $0 <repeatmasker_file.out>"
+    exit
+fi
+
+outdir="05_bed_files"
+file=$1
+name=$(basename $(echo $file | perl -pe 's///'))
+
+if [ ${file##*.} == "gz" ]
+then
+    gunzip -c $file | awk -v OFS="\t" '(NR > 3){
+        print $5, $6, $7, $11 "=" $10, ".", $9
+    }' > ${outdir}/${name}.bed
+else
+    cat $file | awk -v OFS="\t" '(NR > 3){
+        print $5, $6, $7, $11 "=" $10, ".", $9
+    }' > ${outdir}/${name}.bed
+fi
